f1c12a07777d9624f31826c7f022d2865616b533
Alper Özışık <alper.ozisik@smartface.io>
2017-08-07 10:23:38 +0000

    added install/uninstall scripts

---
+++ scripts/postinstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+folder=~/workspace/assets/jet
+if [ -d $folder ]; then rm -rf $folder; fi
+mv ./jet ~/workspace/assets
---
+++ scripts/uninstall.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+folder=~/workspace/assets/jet
+if [ -d $folder ]; then rm -rf $folder; fi
